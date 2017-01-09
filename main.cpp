55dd56b14cee6a91a9d1bfa21966c4fe28957d1c
HarveyYifanLi <liyifan201209@gmail.com>
2017-01-09 15:32:08 -0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,49 +1,69 @@
 //
 //  main.cpp
-//  Handle_Input
+//  Handle_Input_part2
+//
 //  Created by Yifan on 2017-01-09.
 //  Copyright © 2017 Yifan. All rights reserved.
 //
-/* --- Day 2: Bathroom Security ---
- 
- ---part 1---
- 
- You arrive at Easter Bunny Headquarters under cover of darkness. However, you left in such a rush that you forgot to use the bathroom! Fancy office buildings like this one usually have keypad locks on their bathrooms, so you search the front desk for the code.
- 
- "In order to improve security," the document you find says, "bathroom codes will no longer be written down. Instead, please memorize and follow the procedure below to access the bathrooms."
- 
- The document goes on to explain that each button to be pressed can be found by starting on the previous button and moving to adjacent buttons on the keypad: U moves up, D moves down, L moves left, and R moves right. Each line of instructions corresponds to one button, starting at the previous button (or, for the first line, the "5" button); press whatever button you're on at the end of each line. If a move doesn't lead to a button, ignore it.
- 
- You can't hold it much longer, so you decide to figure out the code as you walk to the bathroom. You picture a keypad like this:
- 
- 1 2 3
- 4 5 6
- 7 8 9
- Suppose your instructions are:
- 
- ULL
- RRDDD
- LURDL
- UUUUD
- 
- You start at "5" and move up (to "2"), left (to "1"), and left (you can't, and stay on "1"), so the first button is 1.
- Starting from the previous button ("1"), you move right twice (to "3") and then down three times (stopping at "9" after two moves and ignoring the third), ending up with 9.
- Continuing from "9", you move left, up, right, down, and left, ending with 8.
- Finally, you move up four times (stopping at "2"), then down once, ending with 5.
- So, in this example, the bathroom code is 1985.
- 
- Your puzzle input is the instructions from the document you found at the front desk. What is the bathroom code? */
+/*
+ --- Day 2: Bathroom Security ---
+ --- Part Two ---
+ You finally arrive at the bathroom (it's a several minute walk from the lobby so visitors can behold the many fancy conference rooms and water coolers on this floor) and go to punch in the code. Much to your bladder's dismay, the keypad is not at all like you imagined it. Instead, you are confronted with the result of hundreds of man-hours of bathroom-keypad-design meetings:
+     1
+   2 3 4
+ 5 6 7 8 9
+   A B C
+     D
+ You still start at "5" and stop when you're at an edge, but given the same instructions as above, the outcome is very different:
+ You start at "5" and don't move at all (up and left are both edges), ending at 5.
+ Continuing from "5", you move right twice and down three times (through "6", "7", "B", "D", "D"), ending at D.
+ Then, from "D", you move five more times (through "D", "B", "C", "C", "B"), ending at B.
+ Finally, after five more moves, you end at 3.
+ So, given the actual keypad layout, the code would be 5DB3.
+ Using the same instructions in your puzzle input, what is the correct bathroom code?
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
-/* valid_button_value.cpp is to test whether we should carry out the next new instruction on the current button */
-#include "valid_button_value.h"
 
 using std::cout;
 using std::endl;
 
 const char FILE_NAME[] = "/Users/yifan/Desktop/Instruct_Doc.txt";
+/* set const variables for the specific matrix to be defined in main() */
+const int row_size = 5;
+const int coln_size = 5;
+
+/* function valid_button() checks if we were to execute the next instruction based upon the current button value
+ returning true if it's not gonna go to fall at the edge*/
+
+bool valid_button(char keypad[][coln_size],int r,int c,char ch){
+    
+    if (ch == 'U' && !(keypad[r-1][c] == ' ') && ((r-1)>=0 && (r-1)<=4) &&(c>=0 && c<=4)){
+        
+        return  true;
+    }
+    
+    
+    else if (ch == 'D' && !(keypad[r+1][c] == ' ')&& ((r+1)>=0 && (r+1)<=4) &&(c>=0 && c<=4)){
+        
+        return  true;
+    }
+    
+    else if (ch == 'L' && !(keypad[r][c-1] == ' ')&& (r>=0 && r<=4) &&((c-1)>=0 && (c-1)<=4)){
+        
+        return  true;
+    }
+    
+    else if (ch == 'R' && !(keypad[r][c+1] == ' ')&& (r>=0 && r<=4) &&((c+1)>=0 && (c+1)<=4) ){
+        
+        return  true;
+    }
+    
+    return 0;
+    
+}
 
 int main() {
     
@@ -59,30 +79,63 @@ int main() {
         exit(8);
     }
     
+    char keypad [row_size][coln_size]; // Put the keypad in a 5x5 matrix
+    
+    /* Initialize the matrix with ' ' for each entry */
+    for (int i=0; i != row_size; ++i){
+        
+        for (int j=0; j != coln_size; ++j){
+            keypad[i][j] = ' ';
+        }
+    }
+    
+    /* Define the absolute positions (in the matrix) of those buttons */
+    keypad[0][2] = '1';
+    keypad[1][1] = '2';
+    keypad[1][2] = '3';
+    keypad[1][3] = '4';
+    keypad[2][0] = '5';
+    keypad[2][1] = '6';
+    keypad[2][2] = '7';
+    keypad[2][3] = '8';
+    keypad[2][4] = '9';
+    keypad[3][1] = 'A';
+    keypad[3][2] = 'B';
+    keypad[3][3] = 'C';
+    keypad[4][2] = 'D';
+    
+    
     ch = fgetc(in_file); // Read the first character of the instruction file
-                        // to kick-start the a-loop-and-a-half pattern.
-    int button = 5; /* Starting button value */
+    // to kick-start the a-loop-and-a-half pattern.
+    int r = 2;
+    int c = 0;
+    char button = keypad[r][c];
     
     while (ch!=EOF) {
         
-        
         /* search for newline char */
-        
         while (ch != '\n'){
             
-          if(ch == 'U'&& valid_button_value(button,ch)==true) button -= 3;
+                          /* recall that function valid_button() checks if we were to execute the next instruction */
             
-          else if (ch == 'D'&& valid_button_value(button,ch)==true) button += 3;
+            if (ch == 'U' && valid_button(keypad,r,c,ch)==true)
+                r -= 1;
             
-          else if (ch == 'L'&& valid_button_value(button,ch)==true) button -= 1;
+            else if (ch == 'D' && valid_button(keypad,r,c,ch)==true)
+                r += 1;
             
-          else if (ch == 'R'&& valid_button_value(button,ch)==true) button += 1;
-           
-          else button = button;
+            else if (ch == 'L' && valid_button(keypad,r,c,ch)==true)
+                c -= 1;
             
-          ch = fgetc(in_file); // Read the next character/instruction
+            else if (ch == 'R' && valid_button(keypad,r,c,ch)==true)
+                c += 1;
             
-        }
+            else {};
+            
+            button = keypad[r][c]; // update the button value.
+            ch = fgetc(in_file); // Read the next character/instruction
+            
+        } // end of inner while
         
         cout<<button<<endl;
         
@@ -90,9 +143,20 @@ int main() {
         
     }
     
-    
     fclose(in_file);
-    
     return 0;
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
